5b41875d9e5e461bff1bcbe25017935cc05b5be9
Dylan Nelson <dylannelson58@gmail.com>
2021-02-03 21:25:19 -0700

    Randomize and add wisdoms

--- FortuneCookie.vb
+++ FortuneCookie.vb
@@ -13,24 +13,25 @@ Module FortuneCookie
         Dim randomNumberCount(5) As Integer
         Dim randomValue As Integer
 
-        For i = 1 To 10000
-            randomValue = CInt((3 * Rnd()))     ' Generate random value between 0 and 3.
+        Randomize()
+        randomValue = CInt((3 * Rnd()))     ' Generate random value between 0 and 3.
 
             Select Case randomValue
 
                 Case 0
                     randomNumberCount(0) += 1
-                    Console.WriteLine($"wisdom 1")
-                Case 1
+                Console.WriteLine($"Work done hard is not always hard work done.")
+            Case 1
                     randomNumberCount(1) += 1
-                    Console.WriteLine($"wisdom 2")
-                Case 2
+                Console.WriteLine($"The problems of yesterday show the accomplishments of today.")
+            Case 2
                     randomNumberCount(2) += 1
-                    Console.WriteLine($"wisdom 3")
+                Console.WriteLine($"Time is nothing; Timing is everything.")
 
-            End Select
+        End Select
 
-        Next
+
+
         Console.Read()
     End Sub
 
